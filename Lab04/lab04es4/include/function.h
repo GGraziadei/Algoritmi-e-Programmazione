82069415f12a04798565122d3a73aec472a58a0c
Gianluca <graziadei.gianluca@gmail.com>
2020-10-26 11:04:29 +0100

    gitignore

---
+++ Lab04/lab04es4/include/function.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+
+class function
+{
+    public:
+        function();
+        virtual ~function();
+
+    protected:
+
+    private:
+};
+
+#endif // FUNCTION_H
